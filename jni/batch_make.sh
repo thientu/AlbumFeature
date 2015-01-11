e714dee1ce2091d6295a6506b2f6bf1488f5181e
ZhangTao <ztao81@126.com>
2015-01-12 01:10:09 +0800

    Success compiled version:ZhangTao

--- jni/batch_make.sh
+++ jni/batch_make.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 
-cd FaceDetection
- make
-cd ..
+#cd FaceDetection
+# make
+#cd ..
 
-cd EyeLocation_qh
- make
-cd ..
+#cd EyeLocation_qh
+# make
+#cd ..
 
-cd landmark_sdm
- make
-cd ..
+#cd landmark_sdm
+# make
+#cd ..
 
-cd FaceGabor
- make
-cd ..
+#cd FaceGabor
+# make
+#cd ..
 
 cd mpeg7_feature
  make
